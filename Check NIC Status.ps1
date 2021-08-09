ba795eed042996f4e2525df070dd33ac983412d7
omariomaxwell <67775847+omariomaxwell@users.noreply.github.com>
2021-08-08 20:20:15 -0500

    Powershell script written as a work-a-round for Guest OS randomly losing  network access in ESXI version 6.5.
    The issue that affects the network adapter of the guest OS. In my case, Windows Server 2012r2. The network adapter will randomly lose connection to the network. Recycling the NIC (Disabling and re-enabling or restarting the NIC) resolves the connectivity issue. This PowerShell script restarts all NICs if a Ping test to 1.1.1.1 fails. It can be modified to restart a single network or Ping a different destination address.

---
+++ Check NIC Status.ps1
@@ -0,0 +1,9 @@
+﻿#Ping 1.1.1.1 And Store That Information/Result In A Variable.
+$testnetworkconnection = Test-NetConnection -ComputerName 1.1.1.1 -InformationLevel Detailed
+#Run The Stored Variable Against An If Statement. If The Statement Is Not Equel To A True Value Then Run The Command To Restart All Network Adapters.
+if ($testnetworkconnection.PingSucceeded -ne "True" ) {Restart-NetAdapter -Name *}
+
+
+#One Liner To Achieve The Same Results. You Can Replace The Destination IP Address With Any Desired.
+#You Can Also Get The Name Of The Desire Network Interface Card (NIC) with The Command (Get-netadapter) And Replace As Desired.
+Test-NetConnection -ComputerName 1.1.1.1 -InformationLevel Detailed || Restart-NetAdapter -Name *
